b40dfe911c37ac422b9376f0f40454e263281a58
Lucas Santiago <lucasfsanti@gmail.com>
2021-04-14 18:53:11 -0300

    101 Chefs

---
+++ pages/work/101chefs-baobba.vue
@@ -0,0 +1,168 @@
+<template>
+	<div class="chefs-wrapper" :style="{'color': fontColor}">
+		<div class="chefs-container">
+			<h1>
+				101 Chefs
+			</h1>
+			<h2>
+				A Market place platform to connect cooks to neighbours looking for homemade food.
+			</h2>
+			<h2>
+				Co-Creator | Product & Design
+			</h2>
+
+			<p>
+				April 2016 | Dez 2017
+			</p>
+
+			<p>
+				In 2015, Brazil start facing finance and political crisis, to brazilian, one of the first solution to make extra money, its to sell homemade cook every where, corner of the street, bus, train stations and deliveries.
+			</p>
+
+			<p>
+				101 Chefs was born to empower this community and support them on the most difficult step of the business, recurring customers and revenues.
+			</p>
+
+			<section>
+				<p>
+					101 Chef was created by Baobbá, an « open business » company. One of the most amazing work experience.
+				</p>
+
+				<a href="">
+					<svg class="heart-icon" style="width:1.438rem;height:1.25rem" viewBox="0 0 24 24">
+							<path fill="#EF0000" d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z" />
+					</svg>
+					Click to Know about Baobbá
+				</a>
+			</section>
+
+		</div>
+
+		<div class="video">
+			<iframe width="100%" height="460px" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+
+		<div class="chips-container">
+			<Chip>Entrepreneurship</Chip>
+			<Chip>Strategy</Chip>
+			<Chip>Go-To-Market</Chip>
+			<Chip>Process management</Chip>
+			<Chip>Open business</Chip>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapState, mapMutations } from 'vuex'
+export default Vue.extend({
+	computed: {
+		...mapState(['color', 'fontColor'])
+	},
+
+	created() {
+		this.changeColor('#033756', '#FE9F6A');
+	},
+
+	methods: {
+		...mapMutations(['setColor', 'setFontColor']),
+
+		changeColor(backgroundColor: string, fontColor: string) {
+			this.setColor(backgroundColor);
+			this.setFontColor(fontColor);
+		}
+	}
+})
+</script>
+
+<style scoped>
+@media(max-width: 720px) {
+	.chefs-container {
+		padding: 0 10px;
+	}
+}
+
+.chefs-wrapper {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: 1fr auto;
+	grid-template-areas: 
+		'article video'
+		'chips chips';
+  gap: 1rem;
+}
+
+.chefs-container {
+	display: flex;
+	flex-direction: column;
+	grid-area: article;
+}
+
+.chefs-container > h1 {
+	font-size: 5rem;
+	font-weight: bold;
+}
+
+.chefs-container > h2 {
+	font-size: 2.188rem;
+	font-weight: bold;
+	margin-bottom: 1.25rem;
+}
+
+.chefs-container > h2:nth-child(2) {
+	margin-top: 1.25rem;
+}
+
+.chefs-container > h2:nth-child(3) {
+	font-weight: normal;
+	margin-top: 1.875rem;
+}
+
+.chefs-container> h3 {
+	font-size: 1.563rem;
+	font-weight: bold;
+	margin-top: 2.5rem;
+}
+
+.chefs-container p {
+	font-size: 1.563rem;
+	font-weight: normal;
+	margin: 15px 0;
+}
+
+.chefs-container > section {
+	display: flex;
+	flex-direction: column;
+	border: 2px solid #B24181;
+	border-radius: 5px;
+	min-height: auto;
+	text-align: center;
+	padding: 1.375rem 0;
+	width: 100%;
+}
+
+.chefs-container > section > p {
+	font-weight: bold;
+}
+
+.chefs-container > section > a {
+	font-size: 1.125rem;
+	text-decoration: underline;
+}
+
+.heart-icon {
+	vertical-align: bottom;
+}
+
+.video {
+	grid-area: video;
+}
+
+.chefs-wrapper > .chips-container {
+	display: flex;
+	flex-direction: row;
+	margin-top: 3.125rem;
+	flex-flow: wrap;
+	grid-area: chips;
+}
+</style>
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="work-container">
 		<p :style="{'color': fontColor}">i'm doing</p>
-		<NuxtLink to="/work/knewin" tag="a" @mouseover.native="changeColor('#126FF4', '#FFFFFF')" :style="{'color': fontColor}">Knewin</NuxtLink>
 		<NuxtLink to="/work/bmk-partners" tag="a" @mouseover.native="changeColor('#033756', '#FE9F6A')" :style="{'color': fontColor}">BMK Partners</NuxtLink>
 
 		<p :style="{'color': fontColor}">i have done</p>
+		<NuxtLink to="/work/knewin" tag="a" @mouseover.native="changeColor('#126FF4', '#FFFFFF')" :style="{'color': fontColor}">Knewin</NuxtLink>
 		<NuxtLink to="/work/101chefs-baobba" tag="a" @mouseover.native="changeColor('#560332', '#FFFFFF')" :style="{'color': fontColor}">101 Chefs /Baobba</NuxtLink>
 		<NuxtLink to="/work/vivakey" tag="a" :style="{'color': fontColor}">Vivakey</NuxtLink>
 		<NuxtLink to="/work/psa" tag="a" :style="{'color': fontColor}">PSA</NuxtLink>
@@ -50,12 +50,13 @@ export default Vue.extend({
 	font-size: 1.563rem;
 }
 
-.work-container > p:nth-child(4) {
+.work-container > p:nth-child(3) {
 	margin-top: 3rem;
 }
 
 .work-container > a {
 	font-size: 5rem;
 	font-weight: bold;
+	margin: 10px 0;
 }
 </style>
