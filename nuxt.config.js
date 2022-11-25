7ab2417fe1d816006f5d12971cc5af80e99fa792
Lucas Santiago <lucas.santiago@betha.com.br>
2022-11-25 19:36:06 -0300

    Adicionando favicon e mudando título

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ahmed-kleit-site-2022',
+    title: 'Ahmed Kleit',
     htmlAttrs: {
       lang: 'en',
     },
@@ -16,6 +16,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       {
         rel: 'stylesheet',
